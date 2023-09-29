30884d7328b28dd8cb414c5a3ad08ff95f5ce476
bilawal316 <143614305+bilawal316@users.noreply.github.com>
2023-09-29 19:31:47 +0000

    add ./

---
+++ my-app/app/(route)/page.tsx
@@ -0,0 +1,11 @@
+
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div className="p-4">
+      <UserButton afterSignOutUrl="/"/>
+    </div>
+  
+  )
+}
